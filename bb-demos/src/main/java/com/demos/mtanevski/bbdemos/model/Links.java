d39a2f7d8b6e5e11a3078de1a253e566be2426e5
mtanevski <Marjan.Tanevski@endava.com>
2017-07-06 13:22:08 +0200

    initial commit

---
+++ bb-demos/src/main/java/com/demos/mtanevski/bbdemos/model/Links.java
@@ -0,0 +1,16 @@
+package com.demos.mtanevski.bbdemos.model;
+
+/**
+ * Created by mtanevski on 25-Jun-17.
+ */
+public class Links {
+    private Self self;
+
+    public Self getSelf() {
+        return self;
+    }
+
+    public void setSelf(Self self) {
+        this.self = self;
+    }
+}
---
+++ bb-demos/src/main/java/com/demos/mtanevski/bbdemos/model/Self.java
@@ -0,0 +1,13 @@
+package com.demos.mtanevski.bbdemos.model;
+
+public class Self {
+    private String ref;
+
+    public String getRef() {
+        return ref;
+    }
+
+    public void setRef(String ref) {
+        this.ref = ref;
+    }
+}
