1541a01d3424a0969c8ed2b72a029d2353b91673
mtanevski <Marjan.Tanevski@endava.com>
2017-07-10 11:27:18 +0200

    Added initial commit for showcasing rest repositories - a spring project

---
+++ restrepos-example/src/main/java/com/mtanevski/restrepos/restreposexample/RestreposExampleApplication.java
@@ -0,0 +1,12 @@
+package com.mtanevski.restrepos.restreposexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestreposExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestreposExampleApplication.class, args);
+	}
+}
---
+++ restrepos-example/src/test/java/com/mtanevski/restrepos/restreposexample/RestreposExampleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mtanevski.restrepos.restreposexample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RestreposExampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
